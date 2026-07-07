319e28088d4a43c057eacd770e579b2b7abcaa7c
NEILLOG <zhungei@gmail.com>
2026-07-08 03:04:22 +0800

    Refactor code structure for improved readability and maintainability

--- Controllers/AnnualController.cs
+++ Controllers/AnnualController.cs
@@ -1,9 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
+using TADS_Web.Models.DB;
+using TADS_Web.Service;
 
 namespace TADS_Web.Controllers
 {
     public class AnnualController : BaseController
     {
+        private readonly AnnualMeetingService _annualMeetingService;
+
+        public AnnualController(AnnualMeetingService annualMeetingService)
+        {
+            _annualMeetingService = annualMeetingService;
+        }
+
         public IActionResult Current()
         {
             return View();
@@ -12,6 +21,21 @@ namespace TADS_Web.Controllers
         public IActionResult History()
         {
             return View();
+        }
+
+        /// <summary>歷屆年會內容頁（前台）</summary>
+        public IActionResult Detail(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                return RedirectToAction("History");
+
+            IQueryable<TbAnnualMeeting>? dataList = _annualMeetingService.GetItem(ref _message, id);
+            TbAnnualMeeting? item = dataList?.FirstOrDefault(x => x.IsPublish);
+
+            if (item == null)
+                return RedirectToAction("History");
+
+            return View(item);
         }
     }
 }
